2c7ad6e4cf1945eccce1350267db930492642ab5
beshoynasryz <beshoynasry1@gmail.com>
2024-11-16 05:21:37 +0200

    taliwind install

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,6 @@ const App = () => {
   return (
     <div className='mx-4 sm:max-[10%]'>
       <Navbar/>
-
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,25 +3,21 @@ import {assets} from '../assets/assets'
 import { NavLink } from 'react-router-dom'
 const Navbar = () => {
   return (
-   <>
-    <div>
-      <img src={assets.logo} alt="" />
-    </div>
-        <ul>
+ 
+    <div className='flex items-center justify-between text-sm py-4 mb-5 border-b-gray-400'>
+      <img className='w-44 cursor-pointer' src={assets.logo} alt="" />
+    
+        <ul className='hidden md:flex items-start gap-5'>
           <NavLink >
             <li>home</li>
             <hr />
           </NavLink> 
           <NavLink>
+            <li>All Doctors</li>
+            <hr />
+          </NavLink>
+          <NavLink>
             <li>about</li>
-            <hr />
-          </NavLink>
-          <NavLink>
-            <li>Doctors</li>
-            <hr />
-          </NavLink>
-          <NavLink>
-            <li>About</li>
             <hr />
           </NavLink>
           <NavLink>
@@ -33,7 +29,9 @@ const Navbar = () => {
         <div>
           <button>Create account</button>
         </div>
-   </>
+
+        </div >  
+ 
   )
 }
 
